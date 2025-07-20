2b37d53ecea3d4dafec08639d5f7db93d7d199f6
harhit-1000 <harshitredcolour@gmail.com>
2025-07-20 21:02:26 +0530

    back to verson json added

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,7 @@ connectCloudinary()
 // middleware
 
 app.use(express.json())
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors())
 
 // api endpoints
 app.use('/api/user', userRouter)
